70568ea8ef0410baacfadc5f0b87be0efcaa1f74
Seolha <seolha314@gmail.com>
2023-07-07 17:43:10 +0900

    Fix emoji reaction to show variable length emoji as intended

--- src/features/status/components/status-interaction-bar.tsx
+++ src/features/status/components/status-interaction-bar.tsx
@@ -175,7 +175,7 @@ const StatusInteractionBar: React.FC<IStatusInteractionBar> = ({ status }): JSX.
               return (
                 <Emoji
                   key={i}
-                  className='h-4.5 w-4.5 flex-none'
+                  className='h-4.5 flex-none'
                   emoji={e.name}
                   src={e.url}
                 />
